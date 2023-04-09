9c38afc6d36de4d9e78ad71d338b779f0d98a2f8
rafi <r.rafi1615005@gmail.com>
2023-04-10 00:16:26 +0600

    modified

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-
-const middleware = jsonServer.defaults();
-const port = process.env.PORT || 8000;
-
-server.use(middleware);
+server.use(middlewares);
 server.use(router);
 
-server.listen(port)
+server.listen(port);
